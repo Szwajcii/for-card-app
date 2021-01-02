0d1c6af4f71bc81f5bdce6ee5d2953f99151d42e
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-02 19:26:10 +0100

    Add routes in for card user routing.

--- for-card-app/src/app/for-card-user/for-card-user.routing.ts
+++ for-card-app/src/app/for-card-user/for-card-user.routing.ts
@@ -1,12 +1,30 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ForCardUserComponent} from './for-card-user.component';
+import {PaymentCardComponent} from '../for-card-shared/components/payment-card/payment-card.component';
+import {UserProfileComponent} from '../for-card-shared/components/user-profile/user-profile.component';
+import {HomeComponent} from '../for-card-shared/components/home/home.component';
 
 export const ROUTES: Routes = [
   {
     path: '',
     component: ForCardUserComponent,
-    children: []
+    children: [
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'home', component: HomeComponent},
+      {
+        path: 'payment-cards',
+        children: [
+          {path: '', component: PaymentCardComponent}
+        ]
+      },
+      {
+        path: 'profile',
+        children: [
+          {path: '', component: UserProfileComponent}
+        ]
+      }
+    ]
   }
 ];
 
