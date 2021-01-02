49da5005b04e00fdf1f0b7d010422fd9f78b438a
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-02 19:27:20 +0100

    Init app routing.

--- for-card-app/src/app/app-routing.module.ts
+++ for-card-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from './for-card-shared/services/guards/auth.guard';
+import {ForCardGuard} from './for-card-shared/services/guards/for-card-guard';
+import {ForCardMatcher} from './for-card-shared/services/matcher';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    children: [
+      {
+        canActivate: [ForCardGuard],
+        matcher: ForCardMatcher.forCardUserMatcher,
+        loadChildren: () => import('./for-card-user/for-card-user.module').then(module => module.ForCardUserModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
