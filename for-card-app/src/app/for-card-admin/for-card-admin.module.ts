7f97e9718cff062704c5b36411ec2a36a1994215
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-03 18:17:38 +0100

    Import ForCardMatModule into admin module.

--- for-card-app/src/app/for-card-admin/for-card-admin.module.ts
+++ for-card-app/src/app/for-card-admin/for-card-admin.module.ts
@@ -4,16 +4,21 @@ import {ForCardAdminComponent} from './for-card-admin.component';
 import {RouterModule} from '@angular/router';
 import {ForCardSharedModule} from '../for-card-shared/for-card-shared.module';
 import {ForCardAdminRouting} from './for-card-admin.routing';
+import {ForCardMatModule} from '../for-card-shared/modules/for-card-mat.module';
 
 
 
 @NgModule({
   declarations: [ForCardAdminComponent],
+  exports: [
+    ForCardMatModule
+  ],
   imports: [
     CommonModule,
     RouterModule,
     ForCardSharedModule,
-    ForCardAdminRouting
+    ForCardAdminRouting,
+    ForCardMatModule
   ]
 })
 export class ForCardAdminModule { }
