9278f0250b370526a26b909dc98d6ab2ca433f98
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-03 18:31:39 +0100

    Add fields for payment card form.

---
+++ for-card-app/src/app/for-card-shared/components/payment-card/payment-card-form/payment-card-form-fields.ts
@@ -0,0 +1,54 @@
+import {FormGroupHelper} from '../../../model/form-group-helper.model';
+import {Validators} from '@angular/forms';
+
+export const paymentCardFormFields: FormGroupHelper.Model[] = [
+  {
+    inputName: 'paymentCardProvider',
+    label: 'Payment card provider',
+    validators: [Validators.required],
+    type: 'select',
+    selectOptions: [{label: 'Visa', value: 'Visa'}, {label: 'MasterCard', value: 'MasterCard'}],
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'paymentCardNumber',
+    label: 'Payment card number',
+    validators: [Validators.required, Validators.min(16)],
+    type: 'text',
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'paymentCardHolder',
+    label: 'Payment card holder',
+    validators: [Validators.required],
+    type: 'text',
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'expiryDate',
+    label: 'Expiry date',
+    validators: [Validators.required],
+    type: 'date',
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'cvvCode',
+    label: 'CVV code',
+    validators: [Validators.required],
+    type: 'text',
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'cardActive',
+    label: 'Set payment card as active',
+    validators: [Validators.required],
+    type: 'checkbox',
+    cols: 3,
+    rows: 1
+  }
+];
