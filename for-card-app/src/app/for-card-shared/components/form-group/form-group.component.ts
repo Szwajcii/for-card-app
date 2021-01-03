a69cb56c0b22f59d9c64da4d8133edc8ced42a4b
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-03 18:29:52 +0100

    Finish FormGroup component implementation.

--- for-card-app/src/app/for-card-shared/components/form-group/form-group.component.ts
+++ for-card-app/src/app/for-card-shared/components/form-group/form-group.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormGroupHelper} from '../../model/form-group-helper.model';
 
 @Component({
   selector: 'app-form-group',
   templateUrl: './form-group.component.html',
   styleUrls: ['./form-group.component.scss']
 })
-export class FormGroupComponent implements OnInit {
+export class FormGroupComponent {
 
-  constructor() { }
+  @Input() fg: FormGroup;
+  @Input() gridColumns?: number = 4;
+  @Input() name: string;
+  @Input() controls: FormGroupHelper.ModelControl;
+  @Output() click = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  checkForErrors(fgName: string, fcName: string, error: FormGroupHelper.ErrorValidators, fcNestedName?: string) {
+    const formGroup = this.fg.get(fgName);
+    const formNestedControl = fcNestedName ? formGroup.get(fcNestedName) : formGroup;
+    const formControl = formNestedControl.get(fcName);
+
+    return formControl.errors && formControl.errors[error.validator];
   }
-
 }
