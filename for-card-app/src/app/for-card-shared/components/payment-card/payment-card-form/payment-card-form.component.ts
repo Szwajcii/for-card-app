2c3b9eb1e52af1dac8765b73e6730818e23c8e5a
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-03 18:37:15 +0100

    Implement methods in PaymentCardForm component.

--- for-card-app/src/app/for-card-shared/components/payment-card/payment-card-form/payment-card-form.component.ts
+++ for-card-app/src/app/for-card-shared/components/payment-card/payment-card-form/payment-card-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, EventEmitter, OnInit, Output} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {PaymentCardManagementService} from '../../../resources/payment-card.management.service';
+import {FormGroupHelperService} from '../../../services/form-group-helper.service';
+import {MessageService} from '../../../services/message.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {paymentCardFormFields} from './payment-card-form-fields';
+import {PaymentCard} from '../../../model/payment-card.model';
 
 @Component({
   selector: 'app-payment-card-form',
@@ -7,9 +14,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentCardFormComponent implements OnInit {
 
-  constructor() { }
+  @Output() closeDialog = new EventEmitter();
+  private PAYMENT_CARD_HOLDER = 'paymentCardHolder';
 
-  ngOnInit(): void {
+  isDisabled: boolean;
+  paymentCard: PaymentCard.Model;
+  paymentCardHolder: string;
+  formAction;
+  editCard = false;
+
+  gridColumns = 4;
+  generalForm: FormGroup;
+
+  paymentCardDetailsForm: FormGroup;
+  paymentCardDetailsFormControls = this.formGroupService.addControlToModel(paymentCardFormFields);
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<PaymentCardFormComponent>,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private formGroupService: FormGroupHelperService,
+    private paymentCardService: PaymentCardManagementService
+  ) {
   }
 
+  ngOnInit(): void {
+    if (this.data.card) {
+      this.paymentCard = this.data.card;
+      this.isDisabled = this.data.isDisabled;
+      this.formAction = this.data.formAction;
+      this.editCard = true;
+    }
+
+    this.paymentCardDetailsForm = this.formBuilder.group({
+      ...this.formGroupService.getControlsFromModel(this.paymentCardDetailsFormControls)
+    });
+
+    this.generalForm = this.formBuilder.group({
+      paymentCardDetailsForm: this.paymentCardDetailsForm
+    });
+
+    this.paymentCardDetailsForm.get(this.PAYMENT_CARD_HOLDER).setValue(this.paymentCardHolder);
+
+    if (this.paymentCard) {
+      this.setFormValue(this.paymentCard);
+    }
+
+    if (this.isDisabled) {
+      this.generalForm.disable();
+    }
+  }
+
+  setFormValue(paymentCard: PaymentCard.Model) {
+    this.paymentCardDetailsFormControls
+      .forEach(control => this.paymentCardDetailsForm
+        .get(control.inputName)
+        .setValue(paymentCard[control.inputName])
+      );
+  }
+
+  toggleEdit() {
+    this.isDisabled = !this.isDisabled;
+    if (this.isDisabled) {
+      this.setFormValue(this.paymentCard);
+      this.generalForm.disable();
+    } else {
+      this.generalForm.enable();
+    }
+  }
+
+  onSubmit() {
+    const paymentCard: PaymentCard.Model = {
+      ...this.paymentCardDetailsForm.value
+    };
+
+    this.createOrUpdate(paymentCard);
+  }
+
+  onCancel() {
+    this.dialogRef.close(false);
+  }
+
+  createOrUpdate(paymentCard: PaymentCard.Model) {
+    let action;
+
+    if (this.editCard) {
+      paymentCard.id = this.paymentCard.id;
+      paymentCard.userId = this.paymentCard.userId;
+      paymentCard.createdDate = this.paymentCard.createdDate;
+      paymentCard.modifiedDate = this.paymentCard.modifiedDate;
+      action = this.paymentCardService.update(paymentCard);
+    } else {
+      action = this.paymentCardService.create(paymentCard);
+    }
+
+    action.subscribe(data => {
+      this.messageService.showMessage('Added new payment card!');
+      this.dialogRef.close(true);
+    }, error => {
+      this.messageService.showMessage('Adding new payment card failed!');
+      console.log(error);
+      this.dialogRef.close(false);
+    });
+
+  }
 }
