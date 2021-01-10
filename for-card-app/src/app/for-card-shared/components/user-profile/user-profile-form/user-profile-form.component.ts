07a53d1359e807295fb24a5c965207a521e4d63e
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-10 14:53:10 +0100

    Wrap UserProfileForm component with UserProfile parent component.

--- for-card-app/src/app/for-card-shared/components/user-profile/user-profile-form/user-profile-form.component.ts
+++ for-card-app/src/app/for-card-shared/components/user-profile/user-profile-form/user-profile-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormGroupHelper} from '../../../model/form-group-helper.model';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MessageService} from '../../../services/message.service';
+import {Router} from '@angular/router';
+import {FormGroupHelperService} from '../../../services/form-group-helper.service';
+import {userFormFields} from '../user-form-fields';
+import {addressFormFields} from '../../../model/address-form-fields';
 
 @Component({
   selector: 'app-user-profile-form',
@@ -7,9 +14,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userProfile: any;
+  @Input() userProfileFormFields: FormGroupHelper.Model[];
+  @Output() submitEvent = new EventEmitter();
+
+  isDisabled = true;
+  gridColumns = 4;
+
+  generalForm: FormGroup;
+
+  userProfileDetailsForm: FormGroup;
+  userProfileDetailsFormControls: FormGroupHelper.ModelControl[];
+
+  addressDetailsForm: FormGroup;
+  addressDetailsFormControls: FormGroupHelper.ModelControl[];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private router: Router,
+    private formGroupService: FormGroupHelperService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.userProfileDetailsFormControls = this.formGroupService.addControlToModel(userFormFields);
+    this.addressDetailsFormControls = this.formGroupService.addControlToModel(addressFormFields);
+
+    this.userProfileDetailsForm = this.formBuilder.group({
+      ...this.formGroupService.getControlsFromModel(this.userProfileDetailsFormControls)
+    });
+
+    this.addressDetailsForm = this.formBuilder.group({
+      ...this.formGroupService.getControlsFromModel(this.addressDetailsFormControls)
+    });
+
+    this.generalForm = this.formBuilder.group({
+      userProfileDetailsForm: this.userProfileDetailsForm,
+      addressDetailsForm: this.addressDetailsForm
+    });
+
+    if (this.userProfile) {
+      this.setFormValue(this.userProfile);
+    }
+
+    if (this.isDisabled) {
+      this.generalForm.disable();
+    }
+  }
+
+  setFormValue(userProfile: any) {
+    this.userProfileDetailsFormControls
+      .forEach(control => this.userProfileDetailsForm
+        .get(control.inputName)
+        .setValue(userProfile[control.inputName])
+      );
+
+    this.addressDetailsFormControls
+      .forEach(control => this.addressDetailsForm
+        .get(control.inputName)
+        .setValue(userProfile[control.inputName])
+      );
+  }
+
+  toggleEdit() {
+    this.isDisabled = !this.isDisabled;
+    if (this.isDisabled) {
+      this.setFormValue(this.userProfile);
+      this.generalForm.disable();
+    } else {
+      this.generalForm.enable();
+    }
+  }
+
+  onSubmit() {
+    this.submitEvent.emit({
+      userId: this.userProfile.id,
+      userEmail: this.userProfile.email,
+      profileDetails: this.userProfileDetailsForm,
+      addressDetails: this.addressDetailsForm
+    });
   }
 
 }
--- for-card-app/src/app/for-card-shared/components/user-profile/user-profile.component.ts
+++ for-card-app/src/app/for-card-shared/components/user-profile/user-profile.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, EventEmitter, OnInit, Output} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {Router} from '@angular/router';
-import {FormGroupHelperService} from '../../services/form-group-helper.service';
+import {Component, EventEmitter, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {MessageService} from '../../services/message.service';
-import {FormGroupHelper} from '../../model/form-group-helper.model';
-import {addressFormFields} from '../../model/address-form-fields';
-import {userFormFields} from './user-form-fields';
+import {User} from '../../model/user.model';
+import {UserManagementService} from '../../resources/user-management.service';
+import {Observable} from 'rxjs';
+import {filter, map, mergeMap} from 'rxjs/operators';
+import {SUCCESSFULLY_UPDATED, UNEXPECTED_ERROR, UPDATE_HAS_FAILED} from '../../utils/messages';
 
 @Component({
   selector: 'app-user-profile',
@@ -14,86 +14,65 @@ import {userFormFields} from './user-form-fields';
 })
 export class UserProfileComponent implements OnInit {
 
-  @Input() userProfile: any;
-  @Input() userProfileFormFields: FormGroupHelper.Model[];
-  @Output() submitEvent = new EventEmitter();
-
-  isDisabled = true;
-  gridColumns = 4;
-
-  generalForm: FormGroup;
-
-  userProfileDetailsForm: FormGroup;
-  userProfileDetailsFormControls: FormGroupHelper.ModelControl[];
-
-  addressDetailsForm: FormGroup;
-  addressDetailsFormControls: FormGroupHelper.ModelControl[];
+  isLoading = true;
+  userProfile: User.Model;
+  userId: string;
+  @Input() submitEvent: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(
-    private formBuilder: FormBuilder,
     private messageService: MessageService,
-    private router: Router,
-    private formGroupService: FormGroupHelperService
+    private activatedRoute: ActivatedRoute,
+    private userManagementService: UserManagementService
   ) {
   }
 
   ngOnInit(): void {
-    this.userProfileDetailsFormControls = this.formGroupService.addControlToModel(userFormFields);
-    this.addressDetailsFormControls = this.formGroupService.addControlToModel(addressFormFields);
-
-    this.userProfileDetailsForm = this.formBuilder.group({
-      ...this.formGroupService.getControlsFromModel(this.userProfileDetailsFormControls)
+    this.findUserIdFromContext()
+      .pipe(
+        mergeMap(id => this.userManagementService.findUserById(id)),
+        map(model => {
+          console.log(200, model);
+          return model;
+        })
+      ).subscribe(userModel => {
+      this.userProfile = userModel;
+      this.isLoading = false;
+    }, error => {
+      this.isLoading = false;
+      console.log(error);
+      this.messageService.showMessage(UNEXPECTED_ERROR);
     });
-
-    this.addressDetailsForm = this.formBuilder.group({
-      ...this.formGroupService.getControlsFromModel(this.addressDetailsFormControls)
-    });
-
-    this.generalForm = this.formBuilder.group({
-      userProfileDetailsForm: this.userProfileDetailsForm,
-      addressDetailsForm: this.addressDetailsForm
-    });
-
-    if (this.userProfile) {
-      this.setFormValue(this.userProfile);
-    }
-
-    if (this.isDisabled) {
-      this.generalForm.disable();
-    }
   }
 
-  setFormValue(userProfile: any) {
-    this.userProfileDetailsFormControls
-      .forEach(control => this.userProfileDetailsForm
-        .get(control.inputName)
-        .setValue(userProfile[control.inputName])
-      );
-
-    this.addressDetailsFormControls
-      .forEach(control => this.addressDetailsForm
-        .get(control.inputName)
-        .setValue(userProfile[control.inputName])
-      );
+  findUserIdFromContext(): Observable<string> {
+    if (this.userManagementService.getUserContext() != null) {
+      return this.userManagementService.getUserContext().pipe(
+        filter(userContext => !!userContext),
+        map(userContext => userContext.id));
+    }
+    return this.activatedRoute.params.pipe(map(params => params.id));
   }
 
-  toggleEdit() {
-    this.isDisabled = !this.isDisabled;
-    if (this.isDisabled) {
-      // this.setFormValue(this.userProfile);
-      this.generalForm.disable();
-    } else {
-      this.generalForm.enable();
-    }
-  }
+  onSubmit($event) {
+    this.isLoading = true;
+    const userModel: User.Model = {
+      ...$event.profileDetails.value
+    };
 
-  onSubmit() {
-    if (this.userProfileDetailsForm.invalid) {
-      return;
-    }
-    this.submitEvent.emit({
-      profileDetails: this.userProfileDetailsForm,
-      addressDetails: this.addressDetailsForm
-    });
+    userModel.address = $event.addressDetails.value;
+    userModel.id = $event.userId;
+    userModel.email = $event.userEmail;
+
+    this.userManagementService.update(userModel)
+      .subscribe(resData => {
+        this.isLoading = false;
+        console.log(resData);
+        this.messageService.showMessage(SUCCESSFULLY_UPDATED);
+      }, error => {
+        this.isLoading = false;
+        console.log(error);
+        this.messageService.showMessage(UPDATE_HAS_FAILED);
+      });
+
   }
 }
