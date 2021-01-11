011a62f4531b0f89a7f3d552061589e53e615873
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-11 18:31:21 +0100

    Display all users in the system.

--- for-card-app/src/app/for-card-admin/components/users/users-datatable/users-datatable.component.ts
+++ for-card-app/src/app/for-card-admin/components/users/users-datatable/users-datatable.component.ts
@@ -1,21 +1,41 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-users-datatable',
   templateUrl: './users-datatable.component.html',
   styleUrls: ['./users-datatable.component.scss']
 })
-export class UsersDatatableComponent implements OnInit {
+export class UsersDatatableComponent implements OnInit, AfterViewInit {
 
   public datasource = new MatTableDataSource([]);
 
+  @ViewChild('paginator', {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   @Input() displayedColumns: Array<string>;
+
+  @Input() set data(data: any[]) {
+    this.setDatasource(data);
+  }
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+  }
+
+  setDatasource(data: any[]) {
+    this.datasource = new MatTableDataSource<any>(data);
+    this.datasource.data = data;
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
   }
 
 }
--- for-card-app/src/app/for-card-admin/components/users/users.component.ts
+++ for-card-app/src/app/for-card-admin/components/users/users.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserManagementService} from '../../../for-card-shared/resources/user-management.service';
+import {User} from '../../../for-card-shared/model/user.model';
+import {MessageService} from '../../../for-card-shared/services/message.service';
+import {CONNECTION_PROBLEM} from '../../../for-card-shared/utils/messages';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +10,10 @@ import {UserManagementService} from '../../../for-card-shared/resources/user-man
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+
+  users: User.Model[];
+  loading = true;
+  noRecords = false;
 
   public displayedColumns: Array<string> = [
     'name',
@@ -17,11 +24,22 @@ export class UsersComponent implements OnInit {
   ];
 
   constructor(
-    private userManagementService: UserManagementService
+    private userManagementService: UserManagementService,
+    private messageService: MessageService
   ) {
   }
 
   ngOnInit(): void {
+    this.userManagementService.findAllUsers()
+      .subscribe(resData => {
+        this.loading = false;
+        this.users = resData;
+        this.noRecords = this.users.length === 0;
+      }, error => {
+        console.log(error);
+        this.loading = false;
+        this.messageService.showMessage(CONNECTION_PROBLEM);
+      });
   }
 
 }
