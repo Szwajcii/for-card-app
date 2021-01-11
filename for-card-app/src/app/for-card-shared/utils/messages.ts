e0f791e78bf0c488e7fa0306bdeee70883411135
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-11 08:49:18 +0100

    New type of messages.

--- for-card-app/src/app/for-card-shared/utils/messages.ts
+++ for-card-app/src/app/for-card-shared/utils/messages.ts
@@ -1,5 +1,7 @@
 /* BASE MESSAGES */
 export const SUCCESSFULLY_UPDATED = 'Successfully updated!';
+export const SUCCESSFULLY_DELETED = 'Successfully deleted!';
+export const SUCCESSFULLY_ACTIVATED = 'Successfully activated!';
 
 /* ERRORS */
 export const UNEXPECTED_ERROR = 'Unexpected error has occurred!';
