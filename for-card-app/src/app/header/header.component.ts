66b7255e7430ce6f3373590f36e8b0c7007517f5
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-11 08:50:33 +0100

    Add new links in header. Implement logout behaviour.

--- for-card-app/src/app/header/header.component.ts
+++ for-card-app/src/app/header/header.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from '../for-card-shared/components/auth/auth.service';
+import {Subscription} from 'rxjs';
+import {Role} from '../for-card-shared/model/role.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated = false;
 
-  constructor() { }
+  ForCardAdmin = Role.ADMIN;
+  ForCardUser = Role.USER;
+
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
 
   ngOnInit() {
+    this.userSub = this.authService.userContext.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
   onLogout() {
+    this.authService.logout();
   }
+
 }
