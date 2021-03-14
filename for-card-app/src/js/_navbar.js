e57ed39329986f27676ef5db8afed685710dab64
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-03-14 11:37:29 -0400

    Small changes in navbar behaviour.

--- for-card-app/src/js/_navbar.js
+++ for-card-app/src/js/_navbar.js
@@ -1,7 +1,11 @@
-$(function() {
+$(document).ready(function() {
+  const $menuLinks = $('#menuLinks');
+  const $menuBtn = $('#menuBtn');
+
   $('body').scroll(function() {
     const $navLinks = $('#navLinks');
     const $navContainer = $('#navContainer');
     $navLinks.toggleClass('collapsed', $(this).scrollTop() > $navContainer.height());
+    $menuLinks.toggleClass('hidden', $(this).scrollTop() > $navContainer.height());
   });
 });
